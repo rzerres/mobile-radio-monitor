e2ea8b933d41be6ab83f9140fdd781f1a288aebf
Aleksander Morgado <aleksander@lanedo.com>
2013-08-31 14:16:15 +0200

    app: list available QMI devices

--- src/mrm-app.c
+++ src/mrm-app.c
@@ -14,15 +14,267 @@
  */
 
 #include <config.h>
+#include <gudev/gudev.h>
 
 #include "mrm-app.h"
 #include "mrm-window.h"
+#include "mrm-device.h"
 
 G_DEFINE_TYPE (MrmApp, mrm_app, GTK_TYPE_APPLICATION)
 
 struct _MrmAppPrivate {
-    gpointer unused;
+    /* The UDev client */
+    GUdevClient *udev_client;
+    guint initial_scan_id;
+
+    /* List of devices being added */
+    GList *pending_devices;
+
+    /* List of MrmDevices */
+    GList *devices;
 };
+
+/******************************************************************************/
+
+typedef struct {
+    gchar *device_name;
+    GCancellable *cancellable;
+} PendingDeviceInfo;
+
+static PendingDeviceInfo *
+pending_device_info_find (MrmApp *self,
+                          const gchar *device_name)
+{
+    GList *l;
+
+    for (l = self->priv->pending_devices; l; l = g_list_next (l)) {
+        if (g_str_equal (((PendingDeviceInfo *)l->data)->device_name, device_name))
+            return (PendingDeviceInfo *)l->data;
+    }
+    return NULL;
+}
+
+static void
+pending_device_info_cancel (MrmApp *self,
+                            const gchar *device_name)
+{
+    PendingDeviceInfo *info;
+
+    info = pending_device_info_find (self, device_name);
+    if (info)
+        g_cancellable_cancel (info->cancellable);
+}
+
+static void
+pending_device_info_remove (MrmApp *self,
+                            const gchar *device_name)
+{
+    PendingDeviceInfo *info;
+
+    info = pending_device_info_find (self, device_name);
+    if (!info)
+        return;
+
+    self->priv->pending_devices = g_list_remove (self->priv->pending_devices, info);
+    g_object_unref (info->cancellable);
+    g_free (info->device_name);
+    g_slice_free (PendingDeviceInfo, info);
+}
+
+static void
+pending_device_info_add (MrmApp *self,
+                         const gchar *device_name,
+                         GCancellable *cancellable)
+{
+    PendingDeviceInfo *info;
+
+    info = g_slice_new0 (PendingDeviceInfo);
+    info->device_name = g_strdup (device_name);
+    info->cancellable = g_object_ref (cancellable);
+
+    self->priv->pending_devices = g_list_append (self->priv->pending_devices, info);
+}
+
+/******************************************************************************/
+
+typedef struct {
+    MrmApp *self;
+    GFile *file;
+    GCancellable *cancellable;
+    gchar *device_name;
+} PortAddedContext;
+
+static void
+port_added_context_free (PortAddedContext *ctx)
+{
+    g_free (ctx->device_name);
+    g_object_unref (ctx->cancellable);
+    g_object_unref (ctx->file);
+    g_object_unref (ctx->self);
+    g_slice_free (PortAddedContext, ctx);
+}
+
+static void
+device_new_ready (GObject *source,
+                  GAsyncResult *res,
+                  PortAddedContext *ctx)
+{
+    GError *error = NULL;
+    MrmDevice *device;
+
+    device = mrm_device_new_finish (res, &error);
+    if (!device) {
+        g_warning ("Cannot create MRM device: %s", error->message);
+        g_error_free (error);
+    } else if (g_cancellable_is_cancelled (ctx->cancellable)) {
+        g_warning ("MRM device creation cancelled");
+        g_object_unref (device);
+    } else {
+        /* Add device */
+        ctx->self->priv->devices = g_list_append (ctx->self->priv->devices, device);
+    }
+
+    pending_device_info_remove (ctx->self, ctx->device_name);
+    port_added_context_free (ctx);
+}
+
+static gboolean
+filter_usb_device (GUdevDevice *device)
+{
+    const gchar *subsystem;
+    const gchar *name;
+    const gchar *driver;
+    GUdevDevice *parent = NULL;
+    gboolean filtered = TRUE;
+
+    /* Subsystems: 'usb', 'usbmisc' or 'net' */
+    subsystem = g_udev_device_get_subsystem (device);
+    if (!subsystem || !g_str_has_prefix (subsystem, "usb"))
+        goto out;
+
+    /* Names: if 'usb' or 'usbmisc' only 'cdc-wdm' prefixed names allowed */
+    name = g_udev_device_get_name (device);
+    if (!name || !g_str_has_prefix (name, "cdc-wdm"))
+        goto out;
+
+    /* Drivers: 'qmi_wwan' only */
+    driver = g_udev_device_get_driver (device);
+    if (!driver) {
+        parent = g_udev_device_get_parent (device);
+        if (parent)
+            driver = g_udev_device_get_driver (parent);
+    }
+    if (!driver || !g_str_equal (driver, "qmi_wwan"))
+        goto out;
+
+    /* Not filtered! */
+    filtered = FALSE;
+
+ out:
+    if (parent)
+        g_object_unref (parent);
+    return filtered;
+}
+
+static void
+port_added (MrmApp *self,
+            GUdevDevice *udev_device)
+{
+    PortAddedContext *ctx;
+    gchar *path;
+
+    /* Filter */
+    if (filter_usb_device (udev_device))
+        return;
+
+    path = g_strdup_printf ("/dev/%s", g_udev_device_get_name (udev_device));
+    g_debug ("QMI device file available: %s", path);
+
+    ctx = g_slice_new (PortAddedContext);
+    ctx->self = g_object_ref (self);
+    ctx->device_name = g_strdup (g_udev_device_get_name (udev_device));
+    ctx->file = g_file_new_for_path (path);
+    ctx->cancellable = g_cancellable_new ();
+    pending_device_info_add (self, ctx->device_name, ctx->cancellable);
+
+    mrm_device_new (ctx->file,
+                    ctx->cancellable,
+                    (GAsyncReadyCallback) device_new_ready,
+                    ctx);
+
+    g_free (path);
+}
+
+static void
+port_removed (MrmApp *self,
+              GUdevDevice *udev_device)
+{
+    GList *l;
+
+    /* Remove from device file list */
+    for (l = self->priv->devices; l; l = g_list_next (l)) {
+        MrmDevice *device = MRM_DEVICE (l->data);
+
+        if (g_str_equal (mrm_device_get_name (device), g_udev_device_get_name (udev_device))) {
+            g_debug ("QMI device file unavailable: /dev/%s", g_udev_device_get_name (udev_device));
+            g_object_unref (device);
+            self->priv->devices = g_list_delete_link (self->priv->devices, l);
+            return;
+        }
+    }
+
+    /* In case we were still adding it... */
+    pending_device_info_cancel (self, g_udev_device_get_name (udev_device));
+}
+
+static void
+uevent_cb (GUdevClient *client,
+           const gchar *action,
+           GUdevDevice *device,
+           MrmApp *self)
+{
+    /* Port added */
+    if (g_str_equal (action, "add") ||
+        g_str_equal (action, "move") ||
+        g_str_equal (action, "change")) {
+        port_added (self, device);
+        return;
+    }
+
+    /* Port removed */
+    if (g_str_equal (action, "remove")) {
+        port_removed (self, device);
+        return;
+    }
+
+    /* Ignore other actions */
+}
+
+static gboolean
+initial_scan_cb (MrmApp *self)
+{
+    GList *devices, *iter;
+
+    self->priv->initial_scan_id = 0;
+
+    g_debug ("Scanning usb subsystems...");
+
+    devices = g_udev_client_query_by_subsystem (self->priv->udev_client, "usb");
+    for (iter = devices; iter; iter = g_list_next (iter)) {
+        port_added (self, G_UDEV_DEVICE (iter->data));
+        g_object_unref (G_OBJECT (iter->data));
+    }
+    g_list_free (devices);
+
+    devices = g_udev_client_query_by_subsystem (self->priv->udev_client, "usbmisc");
+    for (iter = devices; iter; iter = g_list_next (iter)) {
+        port_added (self, G_UDEV_DEVICE (iter->data));
+        g_object_unref (G_OBJECT (iter->data));
+    }
+    g_list_free (devices);
+
+    return FALSE;
+}
 
 /******************************************************************************/
 /* Main application window management */
@@ -49,7 +301,6 @@ ensure_main_window (MrmApp *self)
 
     window = mrm_window_new (self);
     gtk_application_add_window (GTK_APPLICATION (self), GTK_WINDOW (window));
-    gtk_window_maximize (GTK_WINDOW (window));
     gtk_widget_show_all (window);
 }
 
@@ -198,10 +449,40 @@ mrm_app_new (void)
 static void
 mrm_app_init (MrmApp *self)
 {
+    const gchar *subsys[] = { "usb", "usbmisc", NULL };
+
     self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self, MRM_TYPE_APP, MrmAppPrivate);
 
     g_set_application_name ("MobileRadioMonitor");
     gtk_window_set_default_icon_name ("mobile-radio-monitor");
+
+    /* Setup UDev client */
+    self->priv->udev_client = g_udev_client_new (subsys);
+    g_signal_connect (self->priv->udev_client, "uevent", G_CALLBACK (uevent_cb), self);
+
+    /* Setup initial scan */
+    self->priv->initial_scan_id = g_idle_add ((GSourceFunc) initial_scan_cb, self);
+}
+
+static void
+dispose (GObject *object)
+{
+    MrmApp *self = MRM_APP (object);
+    GList *l;
+
+    if (self->priv->initial_scan_id != 0) {
+        g_source_remove (self->priv->initial_scan_id);
+        self->priv->initial_scan_id = 0;
+    }
+
+    g_assert (self->priv->pending_devices == NULL);
+
+    g_list_free_full (self->priv->devices, g_object_unref);
+    self->priv->devices = NULL;
+
+    g_clear_object (&self->priv->udev_client);
+
+    G_OBJECT_CLASS (mrm_app_parent_class)->dispose (object);
 }
 
 static void
