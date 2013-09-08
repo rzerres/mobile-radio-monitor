705cdd746880e19791aa88fec31da6f05fc6a529
Aleksander Morgado <aleksander@lanedo.com>
2013-09-09 00:22:28 +0200

    color-icon: new 'MrmColorIcon'

---
+++ src/mrm-color-icon.c
@@ -0,0 +1,255 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details:
+ *
+ * Copyright (C) 2013 Aleksander Morgado <aleksander@gnu.org>
+ *
+ * Based on the Gnome system monitor colour pickers
+ *  Copyright (C) 2007 Karl Lattimer <karl@qdh.org.uk>
+ */
+
+#include <config.h>
+
+#include <gtk/gtk.h>
+#include <cairo.h>
+
+#include "mrm-color-icon.h"
+
+G_DEFINE_TYPE (MrmColorIcon, mrm_color_icon, GTK_TYPE_DRAWING_AREA)
+
+#define MIN_WIDTH 30
+#define MIN_HEIGHT 15
+
+enum
+{
+  PROP_0,
+  PROP_COLOR,
+};
+
+struct _MrmColorIconPrivate {
+    GdkRGBA color;
+    cairo_surface_t *image_buffer;
+};
+
+static void
+render (MrmColorIcon *self)
+{
+    cairo_t *cr;
+    gint width, height;
+
+    cr = gdk_cairo_create (gtk_widget_get_window (GTK_WIDGET (self)));
+
+    gdk_cairo_set_source_rgba (cr, &(self->priv->color));
+    width  = gdk_window_get_width  (gtk_widget_get_window (GTK_WIDGET (self)));
+    height = gdk_window_get_height (gtk_widget_get_window (GTK_WIDGET (self)));
+
+    cairo_paint (cr);
+    cairo_set_line_width (cr, 1);
+    cairo_set_source_rgba (cr, 0, 0, 0, 0.5);
+    cairo_rectangle (cr, 0.5, 0.5, width - 1, height - 1);
+    cairo_stroke (cr);
+    cairo_set_line_width (cr, 1);
+    cairo_set_source_rgba (cr, 1, 1, 1, 0.4);
+    cairo_rectangle (cr, 1.5, 1.5, width - 3, height - 3);
+    cairo_stroke (cr);
+    cairo_destroy (cr);
+}
+
+/******************************************************************************/
+
+GtkWidget *
+mrm_color_icon_new (const GdkRGBA *color)
+{
+    return g_object_new (MRM_TYPE_COLOR_ICON,
+                         "color",   color,
+                         NULL);
+}
+
+static gint
+draw (GtkWidget *widget,
+      cairo_t *cr)
+{
+    render (MRM_COLOR_ICON (widget));
+
+    return FALSE;
+}
+
+static void
+realize (GtkWidget *widget)
+{
+    GTK_WIDGET_CLASS (mrm_color_icon_parent_class)->realize (widget);
+
+    render (MRM_COLOR_ICON (widget));
+}
+
+static void
+get_preferred_width (GtkWidget *widget,
+                     gint      *minimum,
+                     gint      *natural)
+{
+    g_return_if_fail (widget != NULL || minimum != NULL || natural != NULL);
+    g_return_if_fail (MRM_IS_COLOR_ICON (widget));
+
+    *minimum = MIN_WIDTH;
+    *natural = MIN_WIDTH;
+}
+
+static void
+get_preferred_height (GtkWidget *widget,
+                      gint      *minimum,
+                      gint      *natural)
+{
+    g_return_if_fail (widget != NULL || minimum != NULL || natural != NULL);
+    g_return_if_fail (MRM_IS_COLOR_ICON (widget));
+
+    *minimum = MIN_HEIGHT;
+    *natural = MIN_HEIGHT;
+}
+
+static void
+size_allocate (GtkWidget *widget,
+               GtkAllocation *allocation)
+{
+    g_return_if_fail (widget != NULL || allocation != NULL);
+    g_return_if_fail (MRM_IS_COLOR_ICON (widget));
+
+    gtk_widget_set_allocation (widget, allocation);
+
+    if (gtk_widget_get_realized (widget)) {
+        gdk_window_move_resize (gtk_widget_get_window (widget),
+                                allocation->x,
+                                allocation->y,
+                                allocation->width,
+                                allocation->height);
+    }
+}
+
+static void
+unrealize (GtkWidget *widget)
+{
+    GTK_WIDGET_CLASS (mrm_color_icon_parent_class)->unrealize (widget);
+}
+
+static void
+style_set (GtkWidget *widget,
+           GtkStyle *previous_style)
+{
+    GTK_WIDGET_CLASS (mrm_color_icon_parent_class)->style_set (widget, previous_style);
+}
+
+static void
+set_property (GObject *object,
+              guint param_id,
+              const GValue *value,
+              GParamSpec *pspec)
+{
+    MrmColorIcon *self = MRM_COLOR_ICON (object);
+
+    switch (param_id)
+    {
+    case PROP_COLOR: {
+        const GdkRGBA *color;
+
+        color = g_value_get_boxed (value);
+        self->priv->color.red = color->red;
+        self->priv->color.green = color->green;
+        self->priv->color.blue = color->blue;
+        self->priv->color.alpha = color->alpha;
+        break;
+    }
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, param_id, pspec);
+        break;
+    }
+}
+
+static void
+get_property (GObject *object,
+              guint param_id,
+              GValue *value,
+              GParamSpec *pspec)
+{
+  MrmColorIcon *self = MRM_COLOR_ICON (object);
+
+  switch (param_id) {
+  case PROP_COLOR: {
+      GdkRGBA color;
+
+      color.red = self->priv->color.red;
+      color.green = self->priv->color.green;
+      color.blue = self->priv->color.blue;
+      color.alpha = self->priv->color.alpha;
+      g_value_set_boxed (value, &color);
+      break;
+  }
+  default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, param_id, pspec);
+      break;
+    }
+}
+
+static void
+finalize (GObject *object)
+{
+    MrmColorIcon *self = MRM_COLOR_ICON (object);
+
+    cairo_surface_destroy (self->priv->image_buffer);
+    self->priv->image_buffer = NULL;
+
+    G_OBJECT_CLASS (mrm_color_icon_parent_class)->finalize (object);
+}
+
+static void
+mrm_color_icon_init (MrmColorIcon *self)
+{
+    self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
+                                              MRM_TYPE_COLOR_ICON,
+                                              MrmColorIconPrivate);
+
+    self->priv->color.red = 0;
+    self->priv->color.green = 0;
+    self->priv->color.blue = 0;
+    self->priv->image_buffer = NULL;
+
+    g_signal_connect (self,
+                      "draw",
+                      G_CALLBACK (draw),
+                      NULL);
+}
+
+static void
+mrm_color_icon_class_init (MrmColorIconClass * klass)
+{
+    GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+    GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+    g_type_class_add_private (gobject_class, sizeof (MrmColorIconPrivate));
+
+    gobject_class->get_property = get_property;
+    gobject_class->set_property = set_property;
+    gobject_class->finalize = finalize;
+
+    widget_class->get_preferred_width  = get_preferred_width;
+    widget_class->get_preferred_height = get_preferred_height;
+    widget_class->size_allocate = size_allocate;
+    widget_class->realize = realize;
+    widget_class->unrealize = unrealize;
+    widget_class->style_set = style_set;
+
+    g_object_class_install_property (gobject_class,
+                                     PROP_COLOR,
+                                     g_param_spec_boxed ("color",
+                                                         "Current Color",
+                                                         "The selected color",
+                                                         GDK_TYPE_RGBA,
+                                                         G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY));
+}
---
+++ src/mrm-color-icon.h
@@ -0,0 +1,56 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details:
+ *
+ * Copyright (C) 2013 Aleksander Morgado <aleksander@gnu.org>
+ *
+ * Based on the Gnome system monitor colour pickers
+ *  Copyright (C) 2007 Karl Lattimer <karl@qdh.org.uk>
+ */
+
+#ifndef __MRM_COLOR_ICON_H__
+#define __MRM_COLOR_ICON_H__
+
+#include <glib.h>
+#include <gdk/gdk.h>
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define MRM_TYPE_COLOR_ICON            (mrm_color_icon_get_type ())
+#define MRM_COLOR_ICON(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), MRM_TYPE_COLOR_ICON, MrmColorIcon))
+#define MRM_COLOR_ICON_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), MRM_TYPE_COLOR_ICON, MrmColorIconClass))
+#define MRM_IS_COLOR_ICON(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), MRM_TYPE_COLOR_ICON))
+#define MRM_IS_COLOR_ICON_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), MRM_TYPE_COLOR_ICON))
+#define MRM_COLOR_ICON_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), MRM_TYPE_COLOR_ICON, MrmColorIconClass))
+
+typedef struct _MrmColorIcon           MrmColorIcon;
+typedef struct _MrmColorIconClass      MrmColorIconClass;
+typedef struct _MrmColorIconPrivate    MrmColorIconPrivate;
+
+struct _MrmColorIcon
+{
+    GtkDrawingArea widget;
+    MrmColorIconPrivate *priv;
+};
+
+struct _MrmColorIconClass
+{
+    GtkDrawingAreaClass parent_class;
+};
+
+GType mrm_color_icon_get_type (void) G_GNUC_CONST;
+
+GtkWidget *mrm_color_icon_new (const GdkRGBA *color);
+
+G_END_DECLS
+#endif /* __MRM_COLOR_ICON_H__ */
